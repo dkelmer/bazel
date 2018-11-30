698cbce07ad2c2e79e0a0e41893bd63f365ab922
Danna Kelmer <dannark@google.com>
2018-11-30 17:14:33 -0500

    revert more unwanted changes

--- src/main/java/com/google/devtools/build/lib/cmdline/BazelContext.java
+++
@@ -1,8 +0,0 @@
-package com.google.devtools.build.lib.cmdline;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.devtools.build.lib.skylarkinterface.StarlarkContext;
-
-public interface BazelContext extends StarlarkContext {
-  ImmutableMap<RepositoryName, RepositoryName> getRepoRemapping();
-}
--- src/main/java/com/google/devtools/build/lib/skylarkinterface/StarlarkContext.java
+++
@@ -1,8 +0,0 @@
-package com.google.devtools.build.lib.skylarkinterface;
-
-import com.google.common.collect.ImmutableMap;
-
-public interface StarlarkContext {}
-
-
-
